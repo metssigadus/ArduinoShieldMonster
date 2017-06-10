cfe59bc4372c0375a0a7d53aeb07ac2053483565
metssigadus <metssigadus@xyz.ee>
2017-06-11 00:21:51 +0300

    Filez needed for EtherLCDclient.ino

---
+++ Src/Syslog/Glyph.h
@@ -0,0 +1,34 @@
+// Some inverse bitmaps to be loaded into char memory of the LCD
+
+  byte smiley[8] = {
+  0b11111,
+  0b10101,
+  0b11111,
+  0b11011,
+  0b11011,
+  0b01110,
+  0b10001,
+  0b11111
+};
+
+  byte antismiley[8] = {
+  0b11111,
+  0b10101,
+  0b11111,
+  0b11011,
+  0b11111,
+  0b10001,
+  0b01110,
+  0b11111
+};
+
+  byte ipglyph[8] = {
+  0b10000,
+  0b00000,
+  0b10111,
+  0b10101,
+  0b10111,
+  0b10100,
+  0b00100,
+  0b01110
+};
